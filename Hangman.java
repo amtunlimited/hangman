76e9a53f7bd8132a8372859a1b7ad291e7ba7729
Aaron Tagliaboschi <aaron.tagliaboschi@gmail.com>
2015-12-03 00:13:38 -0500

    Initial commit

---
+++ Hangman.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+
+public class Hangman {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("How long is the word? ");
+		int wordlength = in.nextInt();
+		try {
+			Scanner file = new Scanner(new File("wordlist/" + wordlength + ".txt"));
+			HashSet<String> wordList = new HashSet<String>();
+			while(file.hasNext()) {
+				wordList.add(file.next());
+			}
+			/*
+			for(String word : wordList) {
+				System.out.println("\t"+word);
+			}
+			*/
+			
+			
+			SuperTable table;
+			HashSet<Character> letterSet = new HashSet<Character>();
+			
+			for(char letter='a'; letter < 'z'; letter++) {
+				letterSet.add(new Character(letter));
+			}
+			
+			int i;
+			for(i=0; i<6 && wordList.size() > 1; i++) {
+				table = new SuperTable(wordList, wordlength, letterSet);
+				System.out.println("My guess is: "+table.getFreq());
+				System.out.print("How many times is the letter in the word? ");
+				
+				int count = in.nextInt();
+				
+				if(count == 0) {
+					wordList = table.notContains(table.getFreq());
+				} else {
+					int[] pos = new int[count];
+					System.out.print("Please enter the position(s) of the letter (seperated by spaces): ");
+					for(int j=0; j<count; j++)
+						pos[j] = in.nextInt();
+					wordList = table.contains(table.getFreq(), pos);
+				}
+				
+				letterSet.remove(new Character(table.getFreq()));
+			}
+			
+			System.out.println("Guess: " + i + "\nHere are my guesses");
+			
+			for(String word : wordList) {
+				System.out.println("\t"+word);
+			}
+			
+		} catch(IOException e) {
+			System.err.println("Couldn't find the file! Or whatever!");
+		}
+	}
+}
---
+++ SuperTable.java
@@ -0,0 +1,119 @@
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class SuperTable {
+	HashSet<String> wordList;
+	Character freq;
+	HashMap<Character, HashSet<String>>[] table;
+	
+	public SuperTable(HashSet<String> wdlist, int wordlength, HashSet<Character> letterSet) {
+		System.out.print("Building...");
+		wordList = (HashSet<String>) wdlist.clone();
+		
+		//ArrayList tableList = new ArrayList<HashMap<Character, HashSet<String>>>(wordlength);
+		
+		//HashMap<Character, HashSet<String>>[] table = (HashMap<Character, HashSet<String>>[]) tableList.toArray(new HashMap<Character, HashSet<String>>[tableList.size()]);
+		
+		table = (HashMap<Character, HashSet<String>>[]) new HashMap[wordlength];
+		
+		int i;
+		for(i=0; i<wordlength; i++) {
+			table[i] = new HashMap<Character, HashSet<String>>();
+		}
+		
+		HashMap<Character, Integer> letterfreq = new HashMap<Character, Integer>();
+		int totalLetters = 0;
+		
+		for(String word : wordList) {
+			HashMap<Character, Boolean> letterfound = new HashMap<Character, Boolean>();
+			for(i=0; i<wordlength; i++) {
+				Character curr = new Character(word.charAt(i));
+				//If this is the first instance, initalize
+				if(letterfreq.get(curr) == null) 
+					letterfreq.put(curr, new Integer(0));
+				
+				if(letterfound.get(curr) == null) {
+					letterfreq.put(curr, new Integer(letterfreq.get(curr).intValue() + 1));
+					letterfound.put(curr, new Boolean(true));
+					totalLetters++;
+				}
+				
+				//If this is the first instance, initalize
+				if(table[i].get(curr) == null) 
+					table[i].put(curr, new HashSet<String>());
+				
+				table[i].get(curr).add(word);
+			}
+		}
+		
+		/*
+		int best = totalLetters;
+		
+		for(Character curr : letterSet) {
+			if(letterfreq.containsKey(curr)) {
+				int freq = letterfreq.get(curr).intValue();
+				if(freq < best) {
+					freq = curr;
+					best = letterfreq.get(curr).intValue();
+				}
+			}
+		}
+		*/
+		
+		int best = 0;
+		for(Character letterc : letterSet) {
+			char letter = letterc.charValue();
+			int total = 0;
+			for(i=0; i<wordlength; i++)
+				if(table[i].containsKey(new Character(letter)))
+					total += table[i].get(new Character(letter)).size();
+			
+			if(total > best) {
+				best = total;
+				freq = new Character(letter);
+			}
+		}
+		
+		System.out.println("Built");
+	}
+	
+	char getFreq() {return freq.charValue();}
+	
+	HashSet<String> contains(char letter, int[] pos) {
+		System.out.print("Creating...");
+		HashSet newList = new HashSet<String>();
+		int i;
+		for(String word : table[pos[0]].get(new Character(letter))) {
+			boolean containsLetter = true;
+			for(i=0;i<pos.length;i++) {
+				if(word.charAt(pos[i]) != letter) {
+					containsLetter = false;
+					break;
+				}
+			}
+			
+			if(containsLetter) {
+				newList.add(word);
+			}
+		}
+		System.out.println("Created");
+		return newList;
+	}
+	
+	HashSet<String> notContains(char letter) {
+		System.out.print("Creating...");
+
+		int i;
+		HashSet<String> newList = (HashSet<String>) wordList.clone();
+		for(i=0; i<table.length; i++) {
+			if(table[i].containsKey(new Character(letter)))
+				for(String word : table[i].get(new Character(letter)))
+					newList.remove(word);
+				
+		}
+		System.out.println("Created");
+		
+		return newList;
+	}
+}
